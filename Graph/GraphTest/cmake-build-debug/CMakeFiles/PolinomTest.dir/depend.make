d9fbe29e4fa4af0d7f7fde8b483e8efeed0c9080
COMPUKTER\Compucter <artem.kalnei@gmail.com>
2018-12-07 21:32:28 +0700

    HypernetTest moved to separate repository. Remove unnecessary directories.

--- Graph/GraphTest/cmake-build-debug/CMakeFiles/PolinomTest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for PolinomTest.
-# This may be replaced when dependencies are built.
--- Hypernet/HypernetTest/Funcs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Branch.h"
-#include "Hypernet.h"
-
-Branch PairConnectivity(H &H, Branch &pseudoEdge);
-// debug
-void GenCombinations(const H& H, const std::vector<Branch>& branchList, Branch& sum, std::vector<bool>& branchMask,
-                     int& curPos);
